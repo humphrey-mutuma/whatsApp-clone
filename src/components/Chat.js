90cb13b3df2728a9862d47e5afc0f63b7fa156e2
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-08 21:20:02 +0300

    Successfully enable multiple people send message.

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -73,7 +73,11 @@ const Chat = () => {
       </div>
       <div className="chat_body">
         {messages.map((message) => (
-          <p className={`chat_message ${true && "chat_receiver"}`}>
+          <p
+            className={`chat_message ${
+              message.name === user.displayName && "chat_receiver"
+            }`}
+          >
             <span className="chat_name">{message.name}</span>
             {message.message}
             <span className="chat_timestamp">
