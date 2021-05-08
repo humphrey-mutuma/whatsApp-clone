9297f76dd7d237fae7138eb9df0b0801bffbfbbd
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-08 09:39:33 +0300

    Generate random avator with avator API

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,8 +1,14 @@
 import { Avatar } from '@material-ui/core'
+import { useState, useEffect } from "react";
 import './SidebarChat.css'
 
 const SidebarChat = () => {
-  
+  const [seed, setSeed] = useState('');
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000))
+  }, [])
+
   return (
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
